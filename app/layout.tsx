dd4ecc0e8849350a28e3dbc8020d39ba97425549
Nadernada <nadernada604@gmail.com>
2024-12-11 23:23:57 +0100

    LTitle and description meta tags added

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,12 @@ export default function RootLayout({
         href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
       />
 
+<meta property="og:title" content="Bilco Professional Line: Custom-Colored Concrete Bricks"/>
+    <meta property="og:image" content="./images/OG-image.png"/>
+    <meta property="og:site_name" content="Bilco Brick"/>
+    <meta property="og:description"
+          content="Discover Bilco's new Professional Line. Create custom concrete bricks colored to the core in any hue—green, purple, red or other unique shades. Perfect for personalized commercial projects with exceptional durability."/>
+
       </head>
 
       <body
