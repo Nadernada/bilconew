6461a346d2abdaf500fc0a35ae8e9bdf9658effb
Nadernada <nadernada604@gmail.com>
2024-12-24 16:15:00 +0100

    Implement the contact form

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -96,7 +96,7 @@ const Contact = () => {
 
  
         <div className="mt-12 flex flex-col justify-around bg-contact-gradient rounded-3xl p-3 md:p-12 lg:py-20 w-fit fade-up-contact  -translate-y-14 lg:translate-y-0 max-w-[1300px]">
-          {responseMessage === 'Thank you for contacting us!' && <form className="w-full flex flex-col gap-y-6"  onSubmit={handleSubmit}>
+          {responseMessage !== 'Thank you for contacting us!' && <form className="w-full flex flex-col gap-y-6"  onSubmit={handleSubmit}>
             <input type="text" name="fullname" id="fullname" value={formData.fullname} onChange={handleChange} className="bg-transparent placeholder:text-[#f3f3f6] rounded-none text-[#f3f3f6] placeholder:uppercase border-b text-[1rem] lg:text-sm border-[#969696] w-full leading-7 py-2 colorInput" placeholder="Full Name" />
             <input type="text" name="business_name" id="business_name" value={formData.business_name} onChange={handleChange} className="bg-transparent placeholder:text-[#f3f3f6] rounded-none text-[#f3f3f6] placeholder:uppercase border-b text-[1rem] lg:text-sm border-[#969696] w-full leading-7 py-2 colorInput" placeholder="Business Name" />
             <input type="text" name="business_website" id="business_website" value={formData.business_website} onChange={handleChange} className="bg-transparent placeholder:text-[#f3f3f6] rounded-none text-[#f3f3f6] placeholder:uppercase border-b text-[1rem] lg:text-sm border-[#969696] w-full leading-7 py-2 colorInput" placeholder="Business Website" />
